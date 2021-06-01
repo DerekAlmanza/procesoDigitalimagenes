65e51ea02dd710e6e2382a6ccb84a24976833df8
DerekAlmanza <Derek.Almanza@ciencias.unam.mx>
2021-05-31 19:55:35 -0500

    Filtro letra M a color agregado. Falta opitimizacion.

--- src/practica03/filtroLetras.js
+++ src/practica03/filtroLetras.js
@@ -5,6 +5,43 @@ const letrasBoton = document.getElementById('letras')
  */
 const letras = () => {
     letrasBoton.addEventListener('click', () => {
-        console.log('Boton de filtro letras funciona xd.');
+
+        const porcion = parseInt(prompt(`Ingrese la cantidad de pixeles que quiere tomar para el mosaico: `));
+
+        // Cambiaremos el fondo a un fondo blanco para mejor visualización de la imagen.
+        imgEditada.classList.remove('visualizaciones');
+        imgEditada.classList.add('fondo-blanco');
+
+        // Leemos info
+        const imgInfo = contextoEditado.getImageData(0,0,imgEditada.width,imgEditada.height);
+        const arregloRGB = imgInfo.data;
+
+        const calcularPromedio = (x,y) => {
+            var contador = 0;
+            var rojo = 0; var verde = 0; var azul = 0;
+            for (var j = y; j < y + porcion; j++) {
+                for (var k = x; k < x + porcion; k++) {
+                        var i = (k + j * imgEditada.width) * 4;
+                        rojo += arregloRGB[i];
+                        verde += arregloRGB[i + 1];
+                        azul += arregloRGB[i + 2];
+                        contador++;
+                }
+                rojo = Math.round(rojo / contador);
+                verde = Math.round(verde / contador);
+                azul = Math.round(azul / contador);
+                return [rojo,verde,azul];
+            }
+        }
+
+        for (var y = 0; y < imgEditada.height; y += porcion) {
+            for (var x = 0; x < imgEditada.width; x += porcion) {
+                var rgb = calcularPromedio(x, y);
+                contextoEditado.fillStyle = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+                contextoEditado.fillText("M", x, y);
+                contextoEditado.clearRect(x,y,porcion,porcion);
+            }
+        }
+
     })
 }
